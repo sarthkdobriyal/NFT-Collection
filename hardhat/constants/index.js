3c29d31ae467fd33e3afc8c49de5ac4d5c5ea5c2
high-prog <dobriyalsarthaksa.8@gmail.com>
2023-03-31 01:03:56 +0530

    eloy

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x16b206d84088A626472030e86f30Fa8eD0Bf18b9";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-five-puce.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
